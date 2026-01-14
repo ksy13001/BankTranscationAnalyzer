1eb36c22c87c9a4d604624334488cede5f8cbb0d
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-01-14 11:44:01 +0900

    2.4 KISS 원칙
    - 불변 변수에 final 붙여주기

--- src/BankTransactionAnalyzerSimple.java
+++ src/BankTransactionAnalyzerSimple.java
@@ -1,2 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class BankTransactionAnalyzerSimple {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/BankTransactionDetails";
+        final Path transactionDetails = Path.of(filePath);
+        final List<String> lines = Files.readAllLines(transactionDetails);
+
+        double total = 0d;
+
+        for (final String line : lines) {
+            String[] transaction = line.split(",");
+            double amount = Double.parseDouble(transaction[1]);
+            if (amount > 0) {
+                total += amount;
+            } else {
+                total += amount;
+            }
+        }
+
+        System.out.println("total: " + total);
+    }
 }
